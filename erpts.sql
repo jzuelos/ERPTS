32655b77b654b4beb0f934fb776d782c257f9392
Lloyd <jzuelos@gmail.com>
2024-10-29 20:37:29 +0800

    Add files via upload

--- erpts.sql
+++ erpts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2024 at 04:15 PM
+-- Generation Time: Oct 29, 2024 at 01:28 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,7 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `owners_tb` (
   `own_id` int(30) NOT NULL,
-  `own_name` varchar(50) NOT NULL,
+  `own_fname` varchar(20) NOT NULL,
+  `own_mname` varchar(20) NOT NULL,
+  `own_surname` varchar(20) NOT NULL,
   `tin_no` int(20) NOT NULL,
   `house_no` varchar(20) NOT NULL,
   `street` varchar(30) NOT NULL,
@@ -39,6 +41,14 @@ CREATE TABLE `owners_tb` (
   `province` varchar(20) NOT NULL,
   `own_info` varchar(70) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `owners_tb`
+--
+
+INSERT INTO `owners_tb` (`own_id`, `own_fname`, `own_mname`, `own_surname`, `tin_no`, `house_no`, `street`, `barangay`, `district`, `city`, `province`, `own_info`) VALUES
+(1, 'zuelos', '', 'john lloyd', 0, '', 'luzaragga', 'kalamunding', '2', 'labo', 'camarines norte', ''),
+(2, 'melanie', '', 'zuelos', 0, '', 'suzara', 'gumamela', '2', 'daet', 'camarines sur', '');
 
 -- --------------------------------------------------------
 
@@ -65,7 +75,14 @@ CREATE TABLE `p_info` (
 --
 
 INSERT INTO `p_info` (`p_id`, `house_no`, `block_no`, `province`, `city`, `district`, `barangay`, `house_tag_no`, `land_area`, `desc_land`, `documents`) VALUES
-(16, 4234, 2345234, 'Item 2', '(City)', 'Item 2', '(City)', 234, 42453, '2342 4234 42342 ', 'affidavit, barangay, land_tagg');
+(16, 4234, 2345234, 'Item 2', '(City)', 'Item 2', '(City)', 234, 42453, '2342 4234 42342 ', 'affidavit, barangay, land_tagg'),
+(17, 0, 0, 'Province', '(City)', 'District', '(City)', 0, 23423, '   ', 'affidavit'),
+(18, 0, 0, 'Province', '(City)', 'District', '(City)', 0, 23, '   ', 'land_tagging'),
+(19, 0, 0, 'Province', '(City)', 'District', '(City)', 0, 213, '   ', 'barangay'),
+(20, 0, 0, 'Province', '(City)', 'District', '(City)', 0, 213, '   ', 'land_tagging'),
+(21, 234, 242134, 'Province 1', 'Province 1', 'District 2', 'District 1', 0, 4234, '   ', 'barangay'),
+(22, 31242, 0, 'Province 1', 'Province 1', 'District 2', 'District 1', 0, 4234, '   ', 'affidavit'),
+(23, 34213, 0, 'Province 2', 'Labo', 'District 2', 'Bautista', 0, 312412, '   ', 'affidavit, barangay');
 
 --
 -- Indexes for dumped tables
@@ -91,13 +108,13 @@ ALTER TABLE `p_info`
 -- AUTO_INCREMENT for table `owners_tb`
 --
 ALTER TABLE `owners_tb`
-  MODIFY `own_id` int(30) NOT NULL AUTO_INCREMENT;
+  MODIFY `own_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `p_info`
 --
 ALTER TABLE `p_info`
-  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
