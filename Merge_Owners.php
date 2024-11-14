667c04e7403bda9abeab306f010a6f6f550f9ea1
Mark Luis <111257842+mlabnerdy@users.noreply.github.com>
2024-11-15 03:12:15 +0800

    Added Merge_Owners

---
+++ Merge_Owners.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="main_layout.css">
+  <link rel="stylesheet" href="Merge_Owners.css">
+  <title>Electronic Real Property Tax System</title>
+</head>
+
+<body>
+  <!-- Header Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
+    <a class="navbar-brand">
+      <img src="images/coconut_.__1_-removebg-preview1.png" width="50" height="50" class="d-inline-block align-top"
+        alt="">
+      Electronic Real Property Tax System
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="Home.php">Home</a>
+        </li>
+        <li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle" href="RPU-Management.php" id="navbarDropdown" role="button"
+            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            RPU Management
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item active" href="Real-Property-Unit-List.php">RPU List</a>
+            <a class="dropdown-item" href="FAAS.php">FAAS</a>
+            <a class="dropdown-item" href="Tax-Declaration.php">Tax Declaration</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="Track.php">Track Paper</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Transaction.php">Transaction</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Reports.php">Reports</a>
+        </li>
+        <li class="nav-item ml-3">
+          <a href="logout.php" class="btn btn-danger">Log Out</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+<!--Main Body-->
+<section class="container mt-5 table-container">
+    <div class="table-title">Merge Owners</div>
+    <div class="table-responsive">
+      <table class="table table-striped table-hover">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col" class="center-input">Choose Person</th>
+            <th scope="col" class="center-input">Show/Hide Properties</th>
+            <th scope="col">Person ID</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Middle Name</th>
+            <th scope="col">Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="center-input"><input type="radio" name="personChoice"></td>
+            <td class="center-input"><input type="checkbox" name="propertyDisplay"></td>
+            <td>1</td>
+            <td>Doe</td>
+            <td>John</td>
+            <td>C</td>
+            <td>123 Main St</td>
+          </tr>
+         <!-- Repeat <tr> for each row of data -->
+        </tbody>
+      </table>
+    </div>
+    <div class="btn-container">
+    <button type="button" class="btn btn-custom">Retain Owners</button>
+</div>
+  </section> 
+
+ <!-- Footer -->
+ <footer class="bg-body-tertiary text-center text-lg-start mt-auto">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2020 Copyright:
+      <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+  </footer>
+
+  <!-- Optional JavaScript -->
+  <script src="http://localhost/ERPTS/main_layout.js"></script>
+  <script src="http://localhost/ERPTS/Own_list.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
