ea8e889cfb5063d3fa8362ef26af59c2886bc877
Lloyd <jzuelos@gmail.com>
2024-11-14 22:02:04 +0800

    Add files via upload

--- erpts.sql
+++ erpts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2024 at 04:30 AM
+-- Generation Time: Nov 14, 2024 at 03:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,100 @@ SET time_zone = "+00:00";
 --
 -- Database: `erpts`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brgy`
+--
+
+CREATE TABLE `brgy` (
+  `brgy_id` int(11) NOT NULL,
+  `brgy_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `brgy`
+--
+
+INSERT INTO `brgy` (`brgy_id`, `brgy_name`) VALUES
+(1, 'Anameam'),
+(2, 'Awitan'),
+(3, 'Baay'),
+(4, 'Bagacay'),
+(5, 'Bagong Silang I'),
+(6, 'Bagong Silang II'),
+(7, 'Bakiad'),
+(8, 'Bautista'),
+(9, 'Bayabas'),
+(10, 'Bayan-bayan'),
+(11, 'Benit'),
+(12, 'Anahaw'),
+(13, 'Gumamela'),
+(14, 'San Francisco'),
+(15, 'Kalamunding'),
+(16, 'Bulhao'),
+(17, 'Cabatuhan'),
+(18, 'Cabusay'),
+(19, 'Calabasa'),
+(20, 'Canapawan'),
+(21, 'Daguit'),
+(22, 'Dalas'),
+(23, 'Dumagmang'),
+(24, 'Exciban'),
+(25, 'Fundado'),
+(26, 'Guinacutan'),
+(27, 'Guisican'),
+(28, 'Iberica'),
+(29, 'Lugui'),
+(30, 'Mabilo I'),
+(31, 'Mabilo II'),
+(32, 'Macogon'),
+(33, 'Mahawan-hawan'),
+(34, 'Malangcao-Basud'),
+(35, 'Malasugui'),
+(36, 'Malatap'),
+(37, 'Malaya'),
+(38, 'Malibago'),
+(39, 'Maot'),
+(40, 'Masalong'),
+(41, 'Matanlang'),
+(42, 'Napaod'),
+(43, 'Pag-Asa'),
+(44, 'Pangpang'),
+(45, 'Pinya'),
+(46, 'San Antonio'),
+(47, 'Santa Cruz'),
+(48, 'Bagong Silang III'),
+(49, 'Submakin'),
+(50, 'Talobatib'),
+(51, 'Tigbinan'),
+(52, 'Tulay Na Lupa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faas`
+--
+
+CREATE TABLE `faas` (
+  `faas_id` int(50) NOT NULL,
+  `propertyowner_id` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`propertyowner_id`)),
+  `pro_id` int(50) NOT NULL,
+  `land_id` int(50) DEFAULT NULL,
+  `plants_id` int(50) DEFAULT NULL,
+  `valuation_id` int(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `faas`
+--
+
+INSERT INTO `faas` (`faas_id`, `propertyowner_id`, `pro_id`, `land_id`, `plants_id`, `valuation_id`) VALUES
+(13, '34', 125, NULL, NULL, NULL),
+(14, '37', 126, NULL, NULL, NULL),
+(15, '38', 126, NULL, NULL, NULL),
+(16, '[41,42]', 128, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -47,7 +141,8 @@ CREATE TABLE `owners_tb` (
 --
 
 INSERT INTO `owners_tb` (`own_id`, `own_fname`, `own_mname`, `own_surname`, `tin_no`, `house_no`, `street`, `barangay`, `district`, `city`, `province`, `own_info`) VALUES
-(1, 'zuelos', '', 'john lloyd', 0, '', 'luzaragga', 'kalamunding', '2', 'labo', 'camarines norte', '');
+(1, 'zuelos', '', 'john lloyd', 0, '', 'luzaragga', 'kalamunding', '2', 'labo', 'camarines norte', ''),
+(7, 'Renz', 'Dionela', 'Dioneda', 4234, '32423', 'asdfasd', 'fasdf', 'asdf', 'fasdf', 'fdasf', 'Telephone: , Fax: , Email: , Website: ');
 
 -- --------------------------------------------------------
 
@@ -66,7 +161,18 @@ CREATE TABLE `propertyowner` (
 --
 
 INSERT INTO `propertyowner` (`pO_id`, `property_id`, `owner_id`) VALUES
-(14, 108, 1);
+(14, 108, 1),
+(17, 111, 1),
+(22, 115, 7),
+(23, 115, 1),
+(33, 125, 7),
+(34, 125, 7),
+(35, 126, 7),
+(36, 126, 1),
+(37, 126, 7),
+(38, 126, 1),
+(41, 128, 7),
+(42, 128, 1);
 
 -- --------------------------------------------------------
 
@@ -75,7 +181,7 @@ INSERT INTO `propertyowner` (`pO_id`, `property_id`, `owner_id`) VALUES
 --
 
 CREATE TABLE `p_info` (
-  `p_id` int(10) NOT NULL,
+  `p_id` int(50) NOT NULL,
   `ownID_Fk` int(50) NOT NULL,
   `house_no` int(10) NOT NULL,
   `block_no` int(10) NOT NULL,
@@ -94,8 +200,12 @@ CREATE TABLE `p_info` (
 --
 
 INSERT INTO `p_info` (`p_id`, `ownID_Fk`, `house_no`, `block_no`, `province`, `city`, `district`, `barangay`, `house_tag_no`, `land_area`, `desc_land`, `documents`) VALUES
-(107, 1, 3213, 0, 'Province 1', 'Labo', 'District 1', 'Kalamunding', 3123, 312, '   ', 'affidavit'),
-(108, 1, 3123, 0, 'Province 1', 'Daet', 'District 1', 'Bautista', 3123, 312, '   ', 'affidavit');
+(108, 1, 3123, 0, 'Province 1', 'Daet', 'District 1', 'Bautista', 3123, 312, '   ', 'affidavit'),
+(111, 1, 412341, 0, 'Province 2', 'Daet', 'District 1', 'Kalamunding', 423, 4324, '   ', 'affidavit, barangay'),
+(115, 7, 4234, 4234, 'Province 1', 'Labo', 'District 1', 'Kalamunding', 123, 3123, '   ', 'affidavit'),
+(125, 7, 4234, 432, 'Province 1', 'Labo', 'District 1', 'Bautista', 0, 4324, '   ', 'barangay'),
+(126, 7, 3123, 31231, 'Province 2', 'Labo', 'District 1', 'Bautista', 3123, 3123, '   ', 'affidavit'),
+(128, 7, 3123, 0, 'Province 2', 'Daet', 'District 1', 'Kalamunding', 312, 312, '   ', 'affidavit');
 
 -- --------------------------------------------------------
 
@@ -138,6 +248,20 @@ INSERT INTO `users` (`user_id`, `username`, `password`, `last_name`, `first_name
 --
 
 --
+-- Indexes for table `brgy`
+--
+ALTER TABLE `brgy`
+  ADD PRIMARY KEY (`brgy_id`);
+
+--
+-- Indexes for table `faas`
+--
+ALTER TABLE `faas`
+  ADD PRIMARY KEY (`faas_id`),
+  ADD KEY `propertyowner_id` (`propertyowner_id`(768)),
+  ADD KEY `pro_id` (`pro_id`);
+
+--
 -- Indexes for table `owners_tb`
 --
 ALTER TABLE `owners_tb`
@@ -170,22 +294,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `brgy`
+--
+ALTER TABLE `brgy`
+  MODIFY `brgy_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `faas`
+--
+ALTER TABLE `faas`
+  MODIFY `faas_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
 -- AUTO_INCREMENT for table `owners_tb`
 --
 ALTER TABLE `owners_tb`
-  MODIFY `own_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `own_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `propertyowner`
 --
 ALTER TABLE `propertyowner`
-  MODIFY `pO_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `pO_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT for table `p_info`
 --
 ALTER TABLE `p_info`
-  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `p_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=129;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -196,6 +332,12 @@ ALTER TABLE `users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `faas`
+--
+ALTER TABLE `faas`
+  ADD CONSTRAINT `pro_id` FOREIGN KEY (`pro_id`) REFERENCES `p_info` (`p_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `propertyowner`
