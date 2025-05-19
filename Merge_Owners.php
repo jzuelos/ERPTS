4026762e66905a6c763a26cef564bce46fdc33d7
Mark Luis <markluisbertillo11@gmail.com>
2025-05-20 01:43:29 +0800

    Merge Fix

--- Merge_Owners.php
+++ Merge_Owners.php
@@ -129,57 +129,22 @@ if ($total_result && $total_result->num_rows > 0) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KyZXEJr+8+6g5K4r53m5s3xmw1Is0J6wBd04YOeFvXOsZTgmYF9flT/qe6LZ9s+0" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
     integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" href="main_layout.css">
+  <link rel="stylesheet" href="header.css">
   <link rel="stylesheet" href="Merge_Owners.css">
   <title>Electronic Real Property Tax System</title>
 </head>
 
 <body>
-  <!-- Header Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
-    <a class="navbar-brand">
-      <img src="images/coconut_.__1_-removebg-preview1.png" width="50" height="50" class="d-inline-block align-top"
-        alt="">
-      Electronic Real Property Tax System
-    </a>
-
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="Home.php">Home</a>
-        </li>
-        <li class="nav-item dropdown active">
-          <a class="nav-link dropdown-toggle" href="RPU-Management.php" id="navbarDropdown" role="button"
-            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            RPU Management
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item active" href="Real-Property-Unit-List.php">RPU List</a>
-            <a class="dropdown-item" href="FAAS.php">FAAS</a>
-            <a class="dropdown-item" href="Tax-Declaration.php">Tax Declaration</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="Track.php">Track Paper</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Transaction.php">Transaction</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="Reports.php">Reports</a>
-        </li>
-        <li class="nav-item ml-3">
-          <a href="logout.php" class="btn btn-danger">Log Out</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+     <?php include 'header.php'; ?>
 <!-- Main Body -->
 <form method="post" action="Merge_Owners.php">
   <section class="container mt-5">
@@ -338,6 +303,8 @@ if ($total_result && $total_result->num_rows > 0) {
   <script src="http://localhost/ERPTS/main_layout.js"></script>
   <script src="http://localhost/ERPTS/Merge_Owners.js"></script>
   <script src="http://localhost/ERPTS/Own_list.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
